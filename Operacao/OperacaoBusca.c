63d97e20b825ca2135020097f9115133e377b38a
kepler <kepler.batista456@gmail.com>
2021-10-11 10:25:58 -0300

    Adicionado busca

--- Operacao/OperacaoBusca.c
+++ Operacao/OperacaoBusca.c
@@ -3,8 +3,40 @@
 #include <string.h>
 
 int OperacaoBusca(char *chave){
+    
 
-    
-    
-return EXIT_SUCCESS;
+    char registro[64];
+    char key[6];
+    FILE *entrada;
+    int RRN = 0;
+    int ByteOffSet;
+
+    if ((entrada = fopen("Dados.dat", "rb")) == NULL){
+
+        fprintf(stderr, "Erro ao abrir o arquivo\n");
+        return EXIT_FAILURE;
+
+    }else{
+
+        while(fgets(registro, sizeof(registro), entrada) != NULL){
+       
+            strtok(key, "|");
+
+            if(strcmp(key, chave)){
+
+            ByteOffSet = RRN*64;
+            printf("registro %s encontrado\n Conteudo = %s (RRN = %i - Byte Offset = %i", key, registro, RRN, ByteOffSet);  
+            
+            return EXIT_SUCCESS;
+        
+        }else{
+            RRN++;
+        }
+    }
+
+  
+    printf("Registro nao encontrado!!!\n");
+    return EXIT_FAILURE;
+}
+
 }
